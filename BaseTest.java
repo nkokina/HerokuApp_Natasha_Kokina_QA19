86c8951da7073af6def46eb573e668e29d641b83
nkokina <mae9077@gmail.com>
2022-06-30 00:37:18 +0300

    Addendum BaseTest.java

---
+++ BaseTest.java
@@ -0,0 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+   protected static WebDriver driver;
+
+
+    @BeforeClass
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+}
