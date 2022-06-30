b905c2e96afa00bee4decb4d0fd14e4791a47505
nkokina <mae9077@gmail.com>
2022-06-30 23:34:49 +0300

    Addendum InputsTest.java

---
+++ InputsTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class InputsTest extends BaseTest {
+
+    @Test
+    public void selectInputsTest() {
+        driver.get("http://the-internet.herokuapp.com/inputs");
+        WebElement inputNumber = driver.findElement(By.tagName("input"));
+        List<WebElement> inputs = driver.findElements(By.tagName("input"));
+        inputs.get(0).sendKeys("5");
+        String expectedInput = "5";
+        String actualInput = inputs.get(0).getAttribute("value");
+        Assert.assertEquals(expectedInput, actualInput, "Number entered is incorrect");
+        inputs.get(0).sendKeys(Keys.ARROW_UP);
+        String expectedInputUp = "6";
+        String actualInputUp = inputs.get(0).getAttribute("value");
+        Assert.assertEquals(expectedInputUp, actualInputUp, "The number has not increased");
+        inputs.get(0).sendKeys("Natasha"); //11
+        inputs.get(0).getAttribute("value");
+        String expectedInputLetters = "6";
+        String actualInputLetters = inputs.get(0).getAttribute("value");
+        Assert.assertEquals(expectedInputLetters, actualInputLetters, "Letters entered");
+
+    }
+}
+
+
