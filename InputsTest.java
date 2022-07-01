a6cf2548f00b71efd33f00128749f030e6579a5a
nkokina <mae9077@gmail.com>
2022-07-01 11:14:47 +0300

    Added corrected project InputsTest.java

--- InputsTest.java
+++ InputsTest.java
@@ -11,19 +11,19 @@ public class InputsTest extends BaseTest {
     @Test
     public void selectInputsTest() {
         driver.get("http://the-internet.herokuapp.com/inputs");
-        List<WebElement> inputs = driver.findElements(By.tagName("input"));
-        inputs.get(0).sendKeys("5");
+        WebElement inputNumber = driver.findElement(By.tagName("input"));
+        inputNumber.sendKeys("5");
         String expectedInput = "5";
-        String actualInput = inputs.get(0).getAttribute("value");
+        String actualInput = inputNumber.getAttribute("value");
         Assert.assertEquals(expectedInput, actualInput, "Number entered is incorrect");
-        inputs.get(0).sendKeys(Keys.ARROW_UP);
+        inputNumber.sendKeys(Keys.ARROW_UP);
         String expectedInputUp = "6";
-        String actualInputUp = inputs.get(0).getAttribute("value");
+        String actualInputUp = inputNumber.getAttribute("value");
         Assert.assertEquals(expectedInputUp, actualInputUp, "The number has not increased");
-        inputs.get(0).sendKeys("Natasha"); //11
-        inputs.get(0).getAttribute("value");
+        inputNumber.sendKeys("Natasha");
+        inputNumber.getAttribute("value");
         String expectedInputLetters = "6";
-        String actualInputLetters = inputs.get(0).getAttribute("value");
+        String actualInputLetters = inputNumber.getAttribute("value");
         Assert.assertEquals(expectedInputLetters, actualInputLetters, "Letters entered");
 
     }
