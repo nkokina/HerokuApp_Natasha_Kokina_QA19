9786cee2000ec651e4a74fec62ac0971695014cb
nkokina <mae9077@gmail.com>
2022-07-14 17:20:49 +0300

    Deleted line 15 and 19

--- FramesTest.java
+++ FramesTest.java
@@ -12,11 +12,9 @@ public class FramesTest extends BaseTest {
         driver.switchTo().frame("mce_0_ifr");
         String frameText = driver.findElement(By.cssSelector("#tinymce p")).getText();
         String expectedResultText = "Your content goes here.";
-        System.out.println(frameText);
         driver.switchTo().defaultContent();
         String hText = driver.findElement(By.cssSelector(".example h3")).getText();
         String expectedResultTitle = "An iFrame containing the TinyMCE WYSIWYG Editor";
-        System.out.println(hText);
         Assert.assertEquals(frameText, expectedResultText, "Table text does not match");
         Assert.assertEquals(hText, expectedResultTitle,
                 "Title text does not match");
